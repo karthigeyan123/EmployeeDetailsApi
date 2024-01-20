0d80988af6f2d8aaaaa4150569dfc88168ca9415
Godwin <godw335@gmail.com>
2024-01-20 16:21:41 +0530

    merge conflict check

--- EmployeeDetails/DAL/EmployeeDetailsService.cs
+++ EmployeeDetails/DAL/EmployeeDetailsService.cs
@@ -11,28 +11,6 @@ namespace EmployeeDetails.DAL
     {
         public bool SaveEmployeeDetails(EmployeeDetailsModel entity, IFormFileCollection files)
         {
-<<<<<<< HEAD
-            List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
-            DbLayer manageSQL = new DbLayer();
-            sqlParameters.Add(new KeyValuePair<string, string>("@inemployeeid", Convert.ToString(entity.EmployeeId)));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inemployeename", entity.EmployeeName));
-            sqlParameters.Add(new KeyValuePair<string, string>("@indepartment", entity.Department));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inaadharnumberasemployeeid",entity.AadharNumber));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inphonenumber", entity.PhoneNumber));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inalterphonenumber", entity.AlternateNumber));
-            sqlParameters.Add(new KeyValuePair<string, string>("@indateofbirth", entity.DateOfBirth));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inaddressbyaadhar", entity.Address));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inemergencycontactpersonname", entity.EmergencyContactPerson));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inemergencycontactnumber", entity.EmergencyContactNumber));
-            //sqlParameters.Add(new KeyValuePair<string, string>("@inrelation", Convert.ToString (entity.Relation)));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inhealthissue", entity.HealthIssue));
-            //sqlParameters.Add(new KeyValuePair<string, string>("@inbeforetreatment", entity.BeforeTreatment));
-            //sqlParameters.Add(new KeyValuePair<string, string>("@inaftertreatment", entity.AfterTreatment));
-            sqlParameters.Add(new KeyValuePair<string, string>("@intreatmentunder", Convert.ToString (entity.TreatmentUnder)));
-            sqlParameters.Add(new KeyValuePair<string, string>("@inemployeecurrentstatus", Convert.ToString (entity.EmployeeCurrentStatus)));
-            sqlParameters.Add(new KeyValuePair<string, string>("@indocument", entity.Document));
-            return manageSQL.InsertData("insertemployedetails", sqlParameters);
-=======
             try
             {
                 List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
@@ -83,7 +61,6 @@ namespace EmployeeDetails.DAL
                 Console.WriteLine(ex.Message);
                 return false;
             }
->>>>>>> 5d3be88dfd2a6cd78f6ff71abb5f43a2f759b3cb
         }
         public bool UpdateEmployeeDetails(int employeeId, EmployeeDetailsUpdateModel entity, IFormFileCollection files)
         {
--- EmployeeDetails/Model/Employeedetails.cs
+++ EmployeeDetails/Model/Employeedetails.cs
@@ -2,27 +2,6 @@
 {
     public class Employeedetails
     {
-<<<<<<< HEAD
-        public class EmployeeDetailsEntity
-            {
-                public int EmployeeId { get; set; }
-                public string EmployeeName { get; set; }
-                public string Department { get; set; }
-                public string AadharNumber { get; set; }
-                public string PhoneNumber { get; set; }
-                public string AlternateNumber { get; set; }
-                public string DateOfBirth { get; set; }
-                public string Address { get; set; }
-                public string EmergencyContactPerson { get; set; }
-                public string EmergencyContactNumber { get; set; }
-               // public int Relation { get; set; }
-                public string HealthIssue { get; set; }
-                //public string BeforeTreatment { get; set; }
-               // public string AfterTreatment { get; set; }
-                public int TreatmentUnder { get; set; }
-                public int EmployeeCurrentStatus { get; set; }
-                public string? Document { get; set; }
-=======
         public class EmployeeDetailsModel
         {
             public string EmployeeName { get; set; }
@@ -38,7 +17,6 @@
             public int TreatmentUnder { get; set; }
             public int EmployeeCurrentStatus { get; set; }
             public List<DocumentModel> Documents { get; set; }
->>>>>>> 5d3be88dfd2a6cd78f6ff71abb5f43a2f759b3cb
         }
 
         public class DocumentModel
